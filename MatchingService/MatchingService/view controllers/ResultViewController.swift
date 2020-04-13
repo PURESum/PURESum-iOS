d4caf7cd1c3efc98a18e157d2df9219470150d9f
jaehui327 <jaehui327@naver.com>
2020-04-13 23:14:18 +0900

    Add: result view

---
+++ MatchingService/MatchingService/view controllers/ResultViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ResultViewController.swift
+//  MatchingService
+//
+//  Created by JHKim on 2020/04/13.
+//  Copyright © 2020 zhi. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    // MARK: - properties
+    
+    // MARK: - IBOutlet
+    
+    // MARK: - IBAction
+    
+    // MARK: - life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Methods
+    
+}
