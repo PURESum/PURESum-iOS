1400fb7212fcb3015392fae9a029259387cd5d70
jaehui327 <jaehui327@naver.com>
2020-04-13 18:22:00 +0900

    Add: appicon

--- MatchingService/MatchingService/supporting files/AppDelegate.swift
+++ MatchingService/MatchingService/supporting files/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // delay launch screen
+        Thread.sleep(forTimeInterval: 2.0)
+        
         return true
     }
 
