5009de104ff3e174f68a815bdc8eae70b4dacefa
jaehui327 <jaehui327@naver.com>
2020-05-24 23:27:06 +0900

    Add: Chat Storyboard

---
+++ MatchingService/MatchingService/view controllers/Chat/ChatRoomViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ChatRoomViewController.swift
+//  MatchingService
+//
+//  Created by JHKim on 2020/05/24.
+//  Copyright © 2020 zhi. All rights reserved.
+//
+
+import UIKit
+
+class ChatRoomViewController: UIViewController {
+
+    // MARK: - properties
+    
+    // MARK: - IBOutlet
+    
+    // MARK: - IBAction
+    
+    // MARK: - life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - Methods
+}
