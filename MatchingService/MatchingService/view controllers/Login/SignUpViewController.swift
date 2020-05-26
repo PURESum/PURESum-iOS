bd9f90fc93bd89983e9e6c7b41739c9cf3df92f8
jaehui327 <jaehui327@naver.com>
2020-05-26 20:55:08 +0900

    Add: SignUp

---
+++ MatchingService/MatchingService/view controllers/Login/SignUpViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SignUpViewController.swift
+//  MatchingService
+//
+//  Created by JHKim on 2020/05/26.
+//  Copyright © 2020 zhi. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    // MARK: - properties
+    
+    // MARK: - IBOutlet
+    
+    // MARK: - IBAction
+    
+    // MARK: - life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Methods
+
+}
